816eb3f0e28dc22e161f404873e31b2d5592f45a
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2023-11-10 13:47:29 +0100

    Remove unnecessary method

--- chatbot.py
+++ chatbot.py
@@ -36,34 +36,3 @@ class ChatBot:
         )
         return response.choices[0].message.content
 
-    def run_bot(self):
-        @self.bot.message_handler(func=lambda m: m.chat.type == "private" and m.content_type == 'text')
-        def private(message):
-            try:
-                if message.chat.id not in chats:
-                    chats[message.chat.id] = Person(message.chat.id)
-                    bot.send_message(message.from_user.id,
-                                     "Welcome to Taha's Assistant bot.\n \nUse /delete to start new conversation or else it will use conversations History to answer. \n\nPlease note that this bot is for educational purposes only!\n\n Enjoy!")
-
-                person = chats.get(message.chat.id)
-                if message.text == "/delete":
-                    person.delete_chat()
-                    return
-                if message.text == "/start":
-                    return
-                if person.is_sum_needed():
-                    chat_summary = generate_response(
-                        [{"role": "user", "content": f"make a summary of this:\n {person.make_list_to_text()}"}])
-                    person.update_chat(chat_summary)
-                    bot.send_message(message.from_user.id, "Das hier ist aber kein Spielzeug.\n LG \n Taha")
-
-                person.messages.append({"role": "user", "content": message.text})
-                response = generate_response(person.messages)
-                bot.send_message(message.from_user.id, response)
-                person.messages.append({"role": "assistant", "content": response})
-                bot.send_message(self.publish_channel, f"{message.chat.first_name} \n {str(person.messages)}")
-            except Exception as e:
-                bot.send_message(self.publish_channel, str(e))
-
-        self.bot.infinity_polling(interval=0)
-
