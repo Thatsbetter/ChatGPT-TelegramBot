12ed349ce834afcc9d0e179d5a9ef0ede324181b
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2023-11-10 14:47:31 +0100

    Extract run method to ChatBot class

--- chatbot.py
+++ chatbot.py
@@ -1,38 +1,94 @@
+import json
+
 import openai
 import telebot
 from telebot import apihelper
-import json
+
 from person import Person
+
 
 class ChatBot:
     def __init__(self, file_path='credentials.json'):
+        self.publish_channel = None
+        self.bot = None
         self.file_path = file_path
         self.initialize_bot()
         self.chats = {}
+        self.max_message_length = 4096
+        self.current_model = "gpt-3.5-turbo"  # Default model
 
     def initialize_bot(self):
         credentials = self.load_credentials()
-        openai.organization = credentials.get("openai_organisation")
+        openai.organization = credentials.get('openai_organisation')
         openai.api_key = credentials.get('openai_api_key')
         self.bot = telebot.TeleBot(credentials.get('telegram_api_key'))
-        self.publish_channel = credentials.get("publish_channel")
-
         apihelper.SESSION_TIME_TO_LIVE = 5 * 60
+        self.publish_channel = credentials.get('publish_channel')
 
     def load_credentials(self):
         with open(self.file_path, 'r') as file:
             return json.load(file)
 
     def check_credentials(self):
-        if None in (openai.api_key, self.bot.token):
+        if None in (openai.api_key, self.bot.token, self.publish_channel, openai.organization, self.publish_channel):
             raise Exception("Credentials not found")
 
     def generate_response(self, prompt):
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+            model=self.current_model,
             messages=prompt,
             n=1,
             temperature=0.5
         )
         return response.choices[0].message.content
 
+    def set_model(self, new_model):
+        # Validate that the new_model is allowed (add more models as needed)
+        allowed_models = ["gpt-3.5-turbo", "gpt-4", "gpt-4-1106-preview"]  # Add more models as needed
+        if new_model not in allowed_models:
+            raise ValueError(f"Invalid model: {new_model}")
+
+        self.current_model = new_model
+        return f"Switched to model: {new_model}"
+
+    def run_bot(self):
+        @self.bot.message_handler(func=lambda m: m.chat.type == "private" and m.content_type == 'text')
+        def private(message):
+            try:
+                chat_id = message.from_user.id
+                person = self.chats.setdefault(message.chat.id, Person(message.chat.id))
+
+                if message.text == "/delete":
+                    person.delete_chat()
+                    return
+                elif message.text == "/start":
+                    self.bot.send_message(self.publish_channel, f"{message.chat.first_name} \n {str(person.messages)}")
+                    return
+                elif message.text.startswith("/use_model"):
+                    new_model = message.text.split(" ")[1]
+                    response = self.set_model(new_model)
+                    self.bot.send_message(chat_id, response)
+                    return
+
+                if person.is_sum_needed():
+                    chat_summary = self.generate_response(
+                        [{"role": "user", "content": f"make a summary of this:\n {person.make_list_to_text()}"}])
+                    person.update_chat(chat_summary)
+                    self.bot.send_message(chat_id, "Das hier ist aber kein Spielzeug.\n LG \n Taha")
+
+                person.messages.append({"role": "user", "content": message.text})
+                response = self.generate_response(person.messages)
+                person.messages.append({"role": "assistant", "content": response})
+                if len(response) > self.max_message_length:
+                    message_chunks = [response[i:i + self.max_message_length] for i in
+                                      range(0, len(response), self.max_message_length)]
+                    for chunk in message_chunks:
+                        self.bot.send_message(chat_id, chunk)
+                else:
+                    self.bot.send_message(chat_id, response)
+                    self.bot.send_message(self.publish_channel, f"{message.chat.first_name} \n {str(person.messages)}")
+
+            except Exception as e:
+                self.bot.send_message(self.publish_channel, str(e))
+
+        self.bot.infinity_polling(interval=0)
--- main.py
+++ main.py
@@ -1,37 +1,5 @@
 from chatbot import ChatBot
-from person import Person
 
 chatbot = ChatBot()
 chatbot.check_credentials()
-
-@chatbot.bot.message_handler(func=lambda m: m.chat.type == "private" and m.content_type == 'text')
-def private(message):
-    try:
-        if message.chat.id not in chatbot.chats:
-            chatbot.chats[message.chat.id] = Person(message.chat.id)
-            chatbot.bot.send_message(message.from_user.id,
-                             "Welcome to Taha's Assistant bot.\n \nUse /delete to start new conversation or else it will use conversations History to answer. \n\nPlease note that this bot is for educational purposes only!\n\n Enjoy!")
-
-        person = chatbot.chats.get(message.chat.id)
-        if message.text == "/delete":
-            person.delete_chat()
-            return
-        if message.text == "/start":
-            return
-        if person.is_sum_needed():
-            chat_summary = generate_response(
-                [{"role": "user", "content": f"make a summary of this:\n {person.make_list_to_text()}"}])
-            person.update_chat(chat_summary)
-            chatbot.bot.send_message(message.from_user.id, "Das hier ist aber kein Spielzeug.\n LG \n Taha")
-
-        person.messages.append({"role": "user", "content": message.text})
-        response = chatbot.generate_response(person.messages)
-        chatbot.bot.send_message(message.from_user.id, response)
-        person.messages.append({"role": "assistant", "content": response})
-        chatbot.bot.send_message(chatbot.publish_channel, f"{message.chat.first_name} \n {str(person.messages)}")
-    except Exception as e:
-        chatbot.bot.send_message(chatbot.publish_channel, str(e))
-
-
-
-chatbot.bot.infinity_polling(interval=0)
+chatbot.run_bot()
