7752278fd3e21415e6f2bcfb69d843d4c8c7a5b0
whoistahito <seyed.taha.amirhosseini@studium.uni-hamburg.de>
2024-01-09 13:24:26 +0100

    Add Speak functionality

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,7 @@ import json
 
 import openai
 from openai import OpenAI
+import requests
 import telebot
 from telebot import apihelper
 
@@ -48,13 +49,29 @@ class ChatBot:
         )
         return response.choices[0].message.content
 
-    def generate_voice(self,text):
+    def generate_voice(self, text):
         response = self.openApi_client.audio.speech.create(
             model="tts-1",
             voice="alloy",
             input=text
         )
         return response
+
+    def get_voice(self, voice_file_id, chat_id):
+        file_info = self.bot.get_file_url(voice_file_id)
+        response = requests.get(file_info)
+        voice_path = f'voice_message_{chat_id}.ogg'
+        with open(voice_path, 'wb') as f:
+            f.write(response.content)
+        return voice_path
+
+    def transcribe(self, voice_path):
+        audio_file = open(voice_path, "rb")
+        result = self.openApi_client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio_file
+        )
+        return result.text
 
     def set_model(self, new_model):
         # Validate that the new_model is allowed (add more models as needed)
@@ -66,10 +83,37 @@ class ChatBot:
         return f"Switched to model: {new_model}"
 
     def run_bot(self):
+        @self.bot.message_handler(content_types=['voice'])
+        def speak(message):
+            try:
+                person = self.chats.setdefault(message.chat.id, Person(message.chat.id))
+                voice_path = self.get_voice(message.voice.file_id, person.chat_id)
+                message_text = self.transcribe(voice_path)
+
+                if person.is_sum_needed():
+                    chat_summary = self.generate_response(
+                        [{"role": "user", "content": f"make a summary of this:\n {person.make_list_to_text()}"}])
+                    person.update_chat(chat_summary)
+
+                person.messages.append({"role": "user", "content": message_text})
+                response = self.generate_response(person.messages)
+                person.messages.append({"role": "assistant", "content": response})
+                voice = self.generate_voice(response)
+                self.bot.send_audio(person.chat_id, voice)
+                if len(person.messages) > self.max_message_length:
+                    message_chunks = [response[i:i + self.max_message_length] for i in
+                                      range(0, len(response), self.max_message_length)]
+                    for chunk in message_chunks:
+                        self.bot.send_message(self.publish_channel,
+                                              f"{message.chat.first_name} \n {str(chunk)}")
+                else:
+                    self.bot.send_message(self.publish_channel, f"{message.chat.first_name} \n {str(person.messages)}")
+            except Exception as e:
+                self.bot.send_message(self.publish_channel, str(e))
+
         @self.bot.message_handler(func=lambda m: m.chat.type == "private" and m.content_type == 'text')
         def private(message):
             try:
-                chat_id = message.from_user.id
                 person = self.chats.setdefault(message.chat.id, Person(message.chat.id))
 
                 if message.text == "/delete":
@@ -81,26 +125,24 @@ class ChatBot:
                 elif message.text.startswith("/use_model"):
                     new_model = message.text.split(" ")[1]
                     response = self.set_model(new_model)
-                    self.bot.send_message(chat_id, response)
+                    self.bot.send_message(person.chat_id, response)
                     return
                 elif message.text.startswith("#speak"):
                     tts = message.text.split("#speak")[1]
                     voice = self.generate_voice(tts)
-                    self.bot.send_voice(chat_id, voice)
+                    self.bot.send_voice(person.chat_id, voice)
                     return
                 elif message.text.startswith("#audio"):
                     tts = message.text.split("#audio")[1]
                     voice = self.generate_voice(tts)
-                    self.bot.send_audio(chat_id, voice)
+                    self.bot.send_audio(person.chat_id, voice)
                     return
-
-
 
                 if person.is_sum_needed():
                     chat_summary = self.generate_response(
                         [{"role": "user", "content": f"make a summary of this:\n {person.make_list_to_text()}"}])
                     person.update_chat(chat_summary)
-                    self.bot.send_message(chat_id, "Das hier ist aber kein Spielzeug.\n LG \n Taha")
+                    self.bot.send_message(person.chat_id, "Das hier ist aber kein Spielzeug.\n LG \n Taha")
 
                 person.messages.append({"role": "user", "content": message.text})
                 response = self.generate_response(person.messages)
@@ -109,9 +151,9 @@ class ChatBot:
                     message_chunks = [response[i:i + self.max_message_length] for i in
                                       range(0, len(response), self.max_message_length)]
                     for chunk in message_chunks:
-                        self.bot.send_message(chat_id, chunk)
+                        self.bot.send_message(person.chat_id, chunk)
                 else:
-                    self.bot.send_message(chat_id, response)
+                    self.bot.send_message(person.chat_id, response)
                     self.bot.send_message(self.publish_channel, f"{message.chat.first_name} \n {str(person.messages)}")
 
             except Exception as e:
